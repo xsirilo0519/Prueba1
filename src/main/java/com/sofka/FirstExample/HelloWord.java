1c5a3b830f4d89c5e884b6c9914bb84f099d8bba
sebas <sebaslpez0519@gmail.com>
2021-11-08 21:06:23 -0500

    Initial commit

---
+++ src/main/java/com/sofka/FirstExample/HelloWord.java
@@ -0,0 +1,35 @@
+package com.sofka.FirstExample;
+
+import java.util.Scanner;
+
+public class HelloWord {
+    public static void main(String[] args) {
+
+        Scanner captura = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero");
+        String numeroStr = captura.nextLine();
+        int numeroInt = Integer.parseInt(numeroStr);
+        int i = numeroStr.length() - 1;
+        int formaNumerica = 0,formaNumerica2 = 0;
+        String formaString = "",formaString2 = "";
+
+        while (i > -1) {
+            //Forma 1: recorriendo String
+            formaString = formaString + Character.getNumericValue(numeroStr.charAt(i));
+            formaNumerica = formaNumerica + Character.getNumericValue(numeroStr.charAt(i));
+            //Forma 2: Residuo
+            formaString2 = formaString2 + numeroInt % 10;
+            formaNumerica2 = formaNumerica2 + numeroInt % 10;
+            numeroInt = numeroInt / 10;
+            i--;
+        }
+        System.out.println("Forma 1: Recorriendo un String\n" +
+                formaNumerica + "\n" +
+                formaString2 + "\n" +
+                "Forma 2: Tomando el residuo de un int\n" +
+                formaNumerica2 + "\n" +
+                formaString2
+        );
+    }
+}
